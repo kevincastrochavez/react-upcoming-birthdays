79cb5dc90341a393d81fe788fb5702e0a1b4669d
Kevin Castro <cas20057@byui.edu>
2024-02-26 23:27:03 -0700

    installed react router and created file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+
 function App() {
-  return <div className='app'>Hello</div>;
+  return (
+    <>
+      <Navigation />
+
+      <BrowserRouter>
+        <Routes>
+          {/* <Route index element={<Home />} />
+    <Route path="about" element={<About />} />
+    <Route path="dashboard" element={<Dashboard />} /> */}
+
+          {/* Using path="*"" means "match anything", so this route
+          acts like a catch-all for URLs that we don't have explicit
+          routes for. */}
+          {/* <Route path="*" element={<NoMatch />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+    <header>
+      <nav></nav>
+    </header>
+  );
+}
+
+export default Navigation;
