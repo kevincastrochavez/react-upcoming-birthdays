6fecdb8fc75a9f6bb7f479e1158ffaa14863f78e
Kevin Castro <cas20057@byui.edu>
2024-03-08 15:06:19 -0700

    created the FriendPreferences component

---
+++ src/components/friendInfo/FriendPreferences.js
@@ -0,0 +1,68 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+import colorIcon from '../../assets/color.svg';
+import toothIcon from '../../assets/tooth.svg';
+import celebrateIcon from '../../assets/celebrate.svg';
+
+const friendInfoCss = css`
+  grid-column: 1/-1;
+  grid-row: 3/4;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 20px;
+`;
+
+const friendPreferenceCss = css`
+  & span {
+    color: #f4bfbf;
+    margin-right: 4px;
+    font-size: 14px;
+  }
+`;
+
+/**
+ * Displays the FriendPreferences component which shows your friend's favorite color, if likes to celebrate and their tooth taste
+ * @param {String} favoriteColor - favorite color
+ * @param {Boolean} likesToCelebrate - true if friend likes to celebrate
+ * @param {String} candyPreference - sweet or sour
+ * @returns {JSX} the FriendPreferences component
+ */
+function FriendPreferences({
+  favoriteColor,
+  likesToCelebrate,
+  candyPreference,
+}) {
+  const celebrateText = likesToCelebrate ? 'Yes' : 'No';
+
+  return (
+    <div css={friendInfoCss}>
+      <div>
+        <div css={friendPreferenceCss}>
+          <span>Color</span>
+          <img src={colorIcon} alt='' />
+        </div>
+
+        <p>{favoriteColor}</p>
+      </div>
+      <div>
+        <div css={friendPreferenceCss}>
+          <span>Tooth</span>
+          <img src={toothIcon} alt='' />
+        </div>
+
+        <p>{candyPreference}</p>
+      </div>
+      <div>
+        <div css={friendPreferenceCss}>
+          <span>Celebrate</span>
+          <img src={celebrateIcon} alt='' />
+        </div>
+
+        <p>{celebrateText}</p>
+      </div>
+    </div>
+  );
+}
+
+export default FriendPreferences;
---
+++ src/components/friendInfo/FriendPreferences.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import FriendPreferences from './FriendPreferences';
+
+describe('FriendPreferences', () => {
+  test('renders with correct props', () => {
+    // Arrange
+    const props = {
+      favoriteColor: 'Blue',
+      likesToCelebrate: true,
+      candyPreference: 'Sour',
+    };
+
+    const { getByText, getByAltText } = render(
+      <FriendPreferences {...props} />
+    );
+
+    const colorElement = getByText(/Color/);
+    const toothElement = getByText(/Tooth/);
+    const celebrateElement = getByText(/Celebrate/);
+
+    // TODO: Figure out how to test SVG icons
+    // const colorIcon = getByAltText('color icon');
+    // const toothIcon = getByAltText('tooth icon');
+    // const celebrateIcon = getByAltText('celebrate icon');
+
+    expect(colorElement).toBeInTheDocument();
+    expect(toothElement).toBeInTheDocument();
+    expect(celebrateElement).toBeInTheDocument();
+
+    // expect(colorIcon).toBeInTheDocument();
+    // expect(toothIcon).toBeInTheDocument();
+    // expect(celebrateIcon).toBeInTheDocument();
+
+    const favoriteColor = getByText(/Blue/);
+    const likesToCelebrate = getByText(/Yes/);
+    const candyPreference = getByText(/Sour/);
+    expect(favoriteColor).toBeInTheDocument();
+    expect(likesToCelebrate).toBeInTheDocument();
+    expect(candyPreference).toBeInTheDocument();
+  });
+});
