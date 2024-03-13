76195658f353477d99712d052a8fa011f8c28567
Kevin Castro <cas20057@byui.edu>
2024-03-12 23:00:05 -0600

    built the AllFriends component

--- src/pages/allFriends/AllFriends.js
+++ src/pages/allFriends/AllFriends.js
@@ -1,11 +1,22 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
 import { css } from '@emotion/react';
 
 import Breadcrumbs from '../../components/breadcrumbs/Breadcrumbs';
+import SearchFriend from '../../components/searchFriend/SearchFriend';
+import SearchResults from '../../components/searchResults/SearchResults';
+import { useFriends, useSearch } from '../../components/BirthdayProvider';
+import MonthFriends from '../../components/monthFriends/MonthFriends';
 
 const mainContainerCss = css`
   padding: 24px;
+  margin-bottom: 60px;
+
+  & > h1 {
+    margin-top: 60px;
+    margin-bottom: 20px;
+    font-size: 24px;
+    font-weight: 400;
+  }
 `;
 
 /**
@@ -13,9 +24,25 @@ const mainContainerCss = css`
  * @returns {JSX.Element}
  */
 function AllFriends() {
+  const { isSearching } = useSearch();
+  const { sortedBirthdaysByMonth } = useFriends();
+
   return (
     <main css={mainContainerCss}>
       <Breadcrumbs />
+      <SearchFriend />
+
+      {isSearching ? (
+        <SearchResults />
+      ) : (
+        <>
+          <h1>All Friends</h1>
+          {sortedBirthdaysByMonth &&
+            sortedBirthdaysByMonth.map((monthObj, index) => (
+              <MonthFriends key={index} monthObj={monthObj} />
+            ))}
+        </>
+      )}
     </main>
   );
 }
