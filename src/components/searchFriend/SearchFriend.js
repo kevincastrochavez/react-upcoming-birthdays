2fc877c7ee4f01bce88dc47110acd26f7d9b5073
Kevin Castro <cas20057@byui.edu>
2024-03-16 18:32:12 -0600

    simplified the SearchFriend component

--- src/components/searchFriend/SearchFriend.js
+++ src/components/searchFriend/SearchFriend.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { TextInput } from '@mantine/core';
 import { IconSearch } from '@tabler/icons-react';
-import { useSearchParams } from 'react-router-dom';
 
 import { useSetSearch } from '../BirthdayProvider';
 
@@ -10,36 +9,17 @@ import { useSetSearch } from '../BirthdayProvider';
  * @returns {JSX.Element}
  */
 function SearchFriend() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const { setIsSearching, setSearchText } = useSetSearch();
+  const { setIsSearching } = useSetSearch();
   const searchIcon = <IconSearch />;
-
-  // console.log(value);
-
-  const onSearchInputFocus = () => {
-    setSearchParams({ searching: 'true' });
-    setIsSearching(true);
-  };
-
-  const onSearchInputBlur = () => {
-    setSearchParams((params) => {
-      params.delete('searching');
-      return params;
-    });
-
-    setIsSearching(false);
-  };
 
   return (
     <TextInput
-      size={'lg'}
+      size={'md'}
       leftSectionPointerEvents='none'
       leftSection={searchIcon}
       placeholder='Search for a Friend'
       radius={10}
-      onFocus={onSearchInputFocus}
-      onBlur={onSearchInputBlur}
-      onChange={(event) => setSearchText(event.currentTarget.value)}
+      onClick={() => setIsSearching(true)}
     />
   );
 }
