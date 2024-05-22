103276f7cd193785a16668776d0f01ed29bbef80
Kevin Castro <kevin.castro.chavez0@gmail.com>
2024-05-22 15:35:30 -0600

    installed confetti package and created function to show fireworks

--- src/helper/utils.js
+++ src/helper/utils.js
@@ -1,3 +1,5 @@
+import confetti from 'canvas-confetti';
+
 /**
  * Gets the number of days until the friend's birthday
  * @param {Object} birthdate - friend's birthday
@@ -39,6 +41,40 @@ function daysUntilBirthday(birthdate) {
     daysToBirthday,
     isBirthdayToday,
   };
+}
+
+/**
+ * Confetti!
+ */
+function fireConfetti() {
+  const duration = 3 * 1000;
+  const animationEnd = Date.now() + duration;
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval = setInterval(function () {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    const particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    });
+    confetti({
+      ...defaults,
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    });
+  }, 250);
 }
 
 /**
@@ -107,8 +143,9 @@ function saveUserUid(key, udi) {
 
 export {
   daysUntilBirthday,
+  fireConfetti,
+  getNextBirthdayAge,
   getUserUid,
   removeUserUid,
   saveUserUid,
-  getNextBirthdayAge,
 };
